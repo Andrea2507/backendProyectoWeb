c296bbefaa3b4cb39473be10fe79177d069b2c0a
Andrea2507 <andrealh2507@umes.edu.gt>
2025-11-08 21:56:41 -0600

    fix: variables manuales para evitar overwrite

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.NAME,
+  process.env.USER,
+  process.env.PASS,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.HOST,
+    port: process.env.PORT_SQL,
     dialect: 'mysql',
     logging: false
   }
